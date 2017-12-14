89200981657e2aca2dfbb97bee18dd2cb756e7a5
Tomáš Vykydal <vyk35227@spseol.cz>
2017-12-14 14:52:26 +0100

    ze skoly 14.12.

--- caesar.py
+++ caesar.py
@@ -5,8 +5,8 @@ Created on Thu Dec  7 12:52:58 2017
 
 @author: vyk35227
 """
-
-
+"""
+spec = "!@#$%^&*(){}[]\=+-\"~"
 n = int(input("Zadej číslo posunu > "))
 m = input("Zadej text > ").upper()
 a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -22,3 +22,25 @@ for i in m:
     f = f + 1
     x = b[a.index(g)]
     print(x, end="")
+"""
+
+
+m = input("Zadej text > ").upper()
+a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+n = input("Zadej klíč > ").upper()
+f = 0
+pocet = len(m)
+
+if " " in m:
+    m = m.replace(" ","")
+    
+for i in n:
+    g = n[f]    #písmeno z klíče
+    y = y[n.index(g)] #klic[klic.index(pismeno_klic)]
+    # pzt = x
+    for i in m:
+        x = m[f]   #písmeno z textu
+        
+        f = f + 1
+        print(y, end="")
+        
